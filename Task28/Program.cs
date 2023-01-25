2a9d9a412465175068707ad18dbd13b2f15d759c
Yuri <telnov1983@gmail.com>
2023-01-25 21:37:24 +0300

    added task28

---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int num)
+{
+    int productnum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        productnum = productnum * i;
+    }
+    return productnum;
+}
+
+Console.Write("Введите целое число: ");
+int number;
+if (int.TryParse(Console.ReadLine(), out number))
+{
+    if (number > 0)
+        Console.WriteLine($"Факториал числа {number} = {Factorial(number)}");
+    else
+        Console.WriteLine("Введено некорректное значение. Введите натуральное число.");
+}
+else
+    Console.WriteLine("Введено некорректное значение. Введите целое число");
