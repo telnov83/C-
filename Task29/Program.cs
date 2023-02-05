f6ac4ce3a563ab81172fe0873a717ed41b240b02
Yuri <telnov1983@gmail.com>
2023-02-05 13:42:22 +0300

    added task29

---
+++ Task29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Write("Введите максимальное количество элементов в массиве: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение эллемента в массиве: ");
+int valueArr = Convert.ToInt32(Console.ReadLine());
+
+int[] mass = GenerateArray(sizeArr, valueArr);
+
+PrintArray(mass);
+Console.Write("->");
+PrintArrayFormating(mass);
+
+int[] GenerateArray(int size, int value)
+{
+    Random random = new Random();
+    int[] arrNumbers = new int [size];
+    for (int i = 0; i < arrNumbers.Length; i++)
+    {
+        arrNumbers[i] = random.Next(0, value);
+    }
+    return arrNumbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void PrintArrayFormating(int[] inArr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < inArr.Length; i++)
+    {
+        Console.Write($"{inArr[i]}");
+        if(i < inArr.Length-1) Console.Write($", ");
+    }
+    Console.Write(" ]");
+}
